ae706095d774643b3e6dbaa961c052b1bb05396e
Evgeniy Koryagin <koryagin.evgeniy@gmail.com>
2017-02-26 12:38:00 +0200

    completed web demo

--- demo.py
+++ demo.py
@@ -1,7 +1,9 @@
+import os
 from speaker_verifier import SpeakerVerifier
 from codecs import open
 import time
 from flask import Flask, render_template, request
+import re
 app = Flask(__name__)
 
 print "Preparing speaker verifier"
@@ -9,23 +11,60 @@ start_time = time.time()
 verifier = SpeakerVerifier()
 print "The model is ready"
 print time.time() - start_time, "seconds"
+test_file_name = 'tmp_test.wav' # PATH for saving wav files that are passed during verification stage
+train_folder_name = verifier.get_train_path()
+
+
+def is_int(x):
+	match = re.search("\D", x)
+	if not match:
+		return True
+	else:
+		return False
 
 @app.route("/speaker-verifier-demo", methods=["POST", "GET"])
-def index_page(text="", prediction_message=""):
-    if request.method == "POST":
-        text = request.form["text"]
-        logfile = open("speaker_demo_logs.txt", "a", "utf-8")
-	print text
-	print >> logfile, "<response>"
-	print >> logfile, text
-        prediction_message = classifier.get_prediction_message(text)
-        print prediction_message
-	print >> logfile, prediction_message
-	print >> logfile, "</response>"
-	logfile.close()
-	
-    return render_template('hello.html', text=text, prediction_message=prediction_message)
+def index_page(prediction="", users=""):
+	users = verifier.get_users()
+	result = ''
+	if request.method == "POST":
+		if request.form['bsubmit'] == 'Reset': 
+			users = verifier.reset_classifier()
+
+		elif request.form['bsubmit'] == 'Upload': # DO TESTING			
+			f = request.files['file_for_test']
+			if f.filename == '':
+				print('No selected file')
+			if f.filename.find('wav')==-1 and f.filename.find('flac')==-1:
+				print('Selected file is not in audio format (WAV or FLAC)!')
+			f.save(test_file_name)
+			result = verifier.verify(test_file_name)
+
+		elif request.form['bsubmit'] == 'Upload files': # DO TRAINING
+			uploaded_files = request.files.getlist("files_for_training")
+			userid = request.form["userid"]
+
+			filenames = []
+			for file in uploaded_files:
+				# Check if the file is one of the allowed types/extensions
+				if file and (file.filename.find('wav')>0 or file.filename.find('flac')>0):
+					filename = file.filename
+					# Move the file form the temporal folder to the upload folder 
+					full_path = os.path.join(train_folder_name, filename)
+					file.save(full_path)
+					# Save the filename into a list, we'll use it later
+					filenames.append(full_path)					
+				else:
+					print('Selected file "%s" is not in WAV or FLAC format!' % file.filename)
+
+			if (is_int(userid)) and (int(userid) in users.keys()):
+				verifier.train_existing_user(filenames, int(userid))
+			else:
+				verifier.train_new_user(filenames, userid)
+				users = verifier.get_users()
+			
+
+	return render_template('hello.html', prediction=result, users=users)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=6006, debug=False)
+	app.run(host='0.0.0.0', port=6006, debug=False)
--- speaker_verifier.py
+++ speaker_verifier.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import soundfile as sf
 
 import matplotlib
 matplotlib.use('Agg')
@@ -17,6 +18,11 @@ import tflearn
 from googlenet import googlenet_core as feature_layer
 from googlenet import googlenet
 
+#To make GPU invisible:
+#export CUDA_VISIBLE_DEVICES=""
+
+#To return to normal:
+#unset CUDA_VISIBLE_DEVICES
 
 class SpeakerVerifier(object):
 
@@ -29,6 +35,7 @@ class SpeakerVerifier(object):
 
 		self.FEATURES_PATH = self.TRAINDATA_PATH + 'features.txt'
 		self.clf_path = self.TRAINDATA_PATH + 'knn.pkl'
+		self.test_img_path = 'temp.png'
 
 		# kNN model params
 		self.n_neighbors = 5
@@ -41,7 +48,10 @@ class SpeakerVerifier(object):
 
 		self.users = self.read_dict()
 		self.model = self.load_model()
+		self.classifier = self.load_clf()
 
+	def get_train_path(self):
+		return self.TRAINDATA_PATH
 
 	def load_model(self):
 		tf.reset_default_graph()
@@ -61,6 +71,12 @@ class SpeakerVerifier(object):
 
 		return m
 
+	def load_clf(self):
+		if os.path.isfile(self.clf_path):
+			return joblib.load(self.clf_path)
+		else:
+			return neighbors.KNeighborsClassifier(self.n_neighbors, weights=self.weights)
+
 	def read_dict(self):
 			if os.path.isfile(self.dict_path):
 				with open(self.dict_path, "r") as f:
@@ -73,28 +89,41 @@ class SpeakerVerifier(object):
 				# return empty dictionary
 				return {}
 
-	def write_dict(dict, filename, sep):
+	def write_dict(self, dict, filename, sep):
 		with open(filename, "w") as f:
 			for i in dict.keys():            
 				f.write(str(i) + sep + dict[i] + "\n")
 	
 
-	def wav_to_flac(filename):
+	def get_users(self):
+		return self.users
+
+	def reset_classifier(self):
+		self.users = {}
+		self.classifier = neighbors.KNeighborsClassifier(self.n_neighbors, weights=self.weights)
+		if os.path.isfile(self.FEATURES_PATH):
+			os.remove(self.FEATURES_PATH)
+		return self.users
+
+
+	def wav_to_flac(self, filename):
+		if filename[-5:]=='.flac':
+			return filename
 		wavdata, samplerate = sf.read(filename)
 		sf.write(filename + '.flac', wavdata, samplerate)
 	
 		return filename + '.flac'
 
 	
-	def get_audio_info(audio_file):
+	def get_audio_info(self, audio_file):
 		""" Reads flac files
 		"""
 		samples, samplerate = sf.read(audio_file)
 		return samples, samplerate
 
 
-	def graph_spectrogram(audio_file, img_path):
-		sound_info, frame_rate = get_audio_info(audio_file)
+	def graph_spectrogram(self, audio_file):
+		sound_info, frame_rate = self.get_audio_info(audio_file)
 		plt.figure(num=None, figsize=(19, 12), frameon=False)
 	
 		fig,ax = plt.subplots(1)
@@ -107,12 +136,13 @@ class SpeakerVerifier(object):
 		plt.axis('tight')
 		plt.axis('off')
 	
-		plt.savefig(img_path) #bbox_inches='tight', pad_inches=0)  
+		plt.savefig(self.test_img_path) #bbox_inches='tight', pad_inches=0)  
 		plt.clf()
 		plt.close()
+		return self.test_img_path
 
 	
-	def get_features(input_path):
+	def get_features(self, input_path):
 		""" Calculates activations of hidden layer of trained NN"""
 
 		img= imread(input_path, mode='RGB')
@@ -133,7 +163,21 @@ class SpeakerVerifier(object):
 
 		for file in files:
 			#print('extracting features from ' + file + ' ...')
-			f = get_features(self.model, file)		
+			f = get_features(file)		
+			if label is not None:
+				f = np.concatenate([[label], f])
+			all_features.append(f)
+
+		return np.array(all_features, dtype=np.float32)
+
+	def extract_feaures_for_files(self, files, label=None):
+	
+		all_features = []
+
+		for file in files:
+			flac_fname = self.wav_to_flac(file)
+			img_fname = self.graph_spectrogram(flac_fname)		
+			f = self.get_features(img_fname)		
 			if label is not None:
 				f = np.concatenate([[label], f])
 			all_features.append(f)
@@ -141,15 +185,15 @@ class SpeakerVerifier(object):
 		return np.array(all_features, dtype=np.float32)
 
 
-	def get_xy_pairs(features):
+	def get_xy_pairs(self, features):
 		y = features[:,0]
 		X = features[:,1:]
 		return X, y
 
 
-	def training_routine(files, label):
-		# calculate feature vectors           !!!!!!!!!!!!
-		feature_set = extract_feaures_for_dir(self.TRAINDATA_PATH, label)
+	def training_routine(self, files, label):
+		# calculate feature vectors
+		feature_set = self.extract_feaures_for_files(files, label)
 
 		
 		# append to existing features
@@ -161,49 +205,62 @@ class SpeakerVerifier(object):
 		# FIT kNN MODEL
 		clf = neighbors.KNeighborsClassifier(self.n_neighbors, weights=self.weights)
 		#clf = neighbors.RadiusNeighborsClassifier(radius = 1.0, weights=weights)
-		X, y = get_xy_pairs(feature_set)
+		X, y = self.get_xy_pairs(feature_set)
 		clf.fit(X, y)
 		joblib.dump(clf, self.clf_path) 
-
+		self.classifier = clf
 
 		# saving features
 		with open(self.FEATURES_PATH,'w') as f:
 			np.savetxt(f, feature_set, fmt='%.10f')
 	
 
-	def train_existing_user(files, user_id):
+	def train_existing_user(self, files, user_id):
 		if len(self.users)==0:
 			return "List of Users is empty!"
 
 		keys = [int(k) for k in self.users.keys()]
-		if not user_id in keys:
+		if not int(user_id) in keys:
 			return "Invalid user ID: " % str(user_id)
 			
-		training_routine(files, user_id)
+		self.training_routine(files, user_id)
 		
 
-	def train_new_user(files, user_name):		
+	def train_new_user(self, files, user_name):		
 		label = 1
 		if len(self.users)>0:
 			keys = [int(k) for k in self.users.keys()]
 			key = np.max(keys)+1
-			users[key]=name
 			global label
 			label = key
+			self.users[label]=user_name.strip()
 		else:
 			self.users = {1: user_name.strip()}
 
 
-		training_routine(files, label)
+		self.training_routine(files, label)
 
 		# save dictionary 
-		if len(self.users.keys())>count:
-			write_dict(self.users, self.dict_path, sep)
+		self.write_dict(self.users, self.dict_path, self.sep)
 
 
-	def verification(files):
+	def verify(self, file):
+		flac_fname = self.wav_to_flac(file)
+		img_fname = self.graph_spectrogram(flac_fname)
+		features = self.get_features(img_fname)
+		answer = int(self.classifier.predict(features.reshape(1,-1))[0])
+		dist, ind = self.classifier.kneighbors(features.reshape(1,-1))
+		if np.min(dist)<900.0:
+			print('Prediction is: %s (%s)' % (answer, self.users[answer]))
+			return '%s (%s)' % (answer, self.users[answer])
+		else:
+			print('Speaker is unknown!')
+			return 'Speaker is unknown!'
+
+
+"""	def verification(self, files):
 		features = extract_feaures_for_dir(self.EVAL_PATH, None)
-		clf = joblib.load(self.clf_path)
+		clf = self.classifier
 		for i in range(len(features)):
 			answer = int(clf.predict(features[i].reshape(1,-1))[0])
 			dist, ind = clf.kneighbors(features[i].reshape(1,-1))
@@ -212,7 +269,7 @@ class SpeakerVerifier(object):
 				return 'Prediction is: %s (%s)' % (answer, self.users[answer])
 			else:
 				#print('Sample is unknown!')
-				return 'Sample is unknown!'
+				return 'Sample is unknown!' """
 
 
 	
